67b20d8dbc13a03c3f75f08788b1778f894358df
KATHIPALLI MADHU KIRAN <119720465+madhukirankathipalli@users.noreply.github.com>
2023-08-12 23:52:03 +0530

    a=int(input()) if a==1:     b="Monday"     print(b) elif a==2:     b="Tuesday"     print(b) elif a==3:     b="Wednesday"     print(b) elif a==4:     b="Thursday"     print(b) elif a==5:     b="Friday"     print(b) elif a==6:     b="Saturday"     print(b) elif a==7:     b="Sunday"     print(b) else :     b="Invalid input"     print(b)

---
+++ Day_name__.py
@@ -0,0 +1,27 @@
+a=int(input())
+if a==1:
+    b="Monday"
+    print(b)
+elif a==2:
+    b="Tuesday"
+    print(b)
+elif a==3:
+    b="Wednesday"
+    print(b)
+elif a==4:
+    b="Thursday"
+    print(b)
+elif a==5:
+    b="Friday"
+    print(b)
+elif a==6:
+    b="Saturday"
+    print(b)
+elif a==7:
+    b="Sunday"
+    print(b)
+else :
+    b="Invalid input"
+    print(b)
+
+
